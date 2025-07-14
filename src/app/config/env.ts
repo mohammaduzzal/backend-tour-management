e112f728c99ade9fed2a3280236c88ee8ba11530
mohammaduzzal <aalp72142@gmail.com>
2025-07-14 18:13:43 +0600

    completed google ligin with the help of passport and change password

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -6,18 +6,25 @@ interface EnvConfig {
     PORT: string,
     DB_URL: string,
     NODE_ENV: "development" | "production",
-    BCRYPT_SALT_ROUND :string,
-    JWT_ACCESS_EXPIRES:string,
-    JWT_ACCESS_SECRET :string,
-    SUPER_ADMIN_EMAIL :string,
-    SUPER_ADMIN_PASSWORD: string
+    BCRYPT_SALT_ROUND: string,
+    JWT_ACCESS_EXPIRES: string,
+    JWT_ACCESS_SECRET: string,
+    JWT_REFRESH_SECRET: string,
+    JWT_REFRESH_EXPIRED: string,
+    SUPER_ADMIN_EMAIL: string,
+    SUPER_ADMIN_PASSWORD: string,
+    GOOGLE_CLIENT_ID:string,
+    GOOGLE_CLIENT_SECRET:string,
+    GOOGLE_CALLBACK_URL:string,
+    EXPRESS_SESSION_SECRET:string,
+    FRONTEND_URL:string
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables : string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD"];
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRED", "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET", "GOOGLE_CALLBACK_URL", "EXPRESS_SESSION_SECRET", "FRONTEND_URL"];
 
-    requiredEnvVariables.forEach(key =>{
-        if(!process.env[key]){
+    requiredEnvVariables.forEach(key => {
+        if (!process.env[key]) {
             throw new Error(`missing require environment variable ${key}`)
         }
     })
@@ -28,10 +35,17 @@ const loadEnvVariables = (): EnvConfig => {
         DB_URL: process.env.DB_URL as string,
         NODE_ENV: process.env.NODE_ENV as "development" | "production",
         BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
-        JWT_ACCESS_EXPIRES:process.env.JWT_ACCESS_EXPIRES as string,
-        JWT_ACCESS_SECRET:process.env.JWT_ACCESS_SECRET as string,
-        SUPER_ADMIN_EMAIL:process.env.SUPER_ADMIN_EMAIL as string,
-        SUPER_ADMIN_PASSWORD:process.env.SUPER_ADMIN_PASSWORD as string
+        JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+        JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+        JWT_REFRESH_EXPIRED: process.env.JWT_REFRESH_EXPIRED as string,
+        SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+        GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID as string,
+        GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET as string,
+        GOOGLE_CALLBACK_URL:process.env.GOOGLE_CALLBACK_URL as string,
+        EXPRESS_SESSION_SECRET:process.env.EXPRESS_SESSION_SECRET as string,
+        FRONTEND_URL:process.env.FRONTEND_URL as string
     }
 }
 
--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -1,8 +1,11 @@
+import  httpStatus  from 'http-status-codes';
 import { NextFunction, Request, Response } from "express";
 import AppError from "../errorHelpers/AppError";
 import { verifyToken } from "../utils/jwt";
 import { envVars } from "../config/env";
 import { JwtPayload } from "jsonwebtoken";
+import { User } from "../modules/user/user.model";
+import { IsActive } from '../modules/user/user.interface';
 
 export const checkAuth = (...authRoles : string[])=>async(req :Request,res:Response,next:NextFunction)=>{
     try {
@@ -12,6 +15,19 @@ export const checkAuth = (...authRoles : string[])=>async(req :Request,res:Respo
             throw new AppError(403, "No Token Received")
         }
     const verifiedToken = verifyToken(accessToken,envVars.JWT_ACCESS_SECRET) as JwtPayload
+
+
+    const isUserExist = await User.findOne({ email: verifiedToken.email })
+
+    if (!isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "user does not exist")
+    }
+    if (isUserExist.isActive === IsActive.BLOCKED || isUserExist.isActive === IsActive.INACTIVE) {
+        throw new AppError(httpStatus.BAD_REQUEST, `user is ${isUserExist.isActive}`)
+    }
+    if (isUserExist.isDelete) {
+        throw new AppError(httpStatus.BAD_REQUEST, "user is deleted")
+    }
 
 
     if(!authRoles.includes(verifiedToken.role)){
